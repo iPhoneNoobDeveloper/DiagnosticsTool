a6873e6c2340e6806ced736b9293458fbea9d73e
Nirav Jain <niravjain02@gmail.com>
2025-10-19 17:50:00 -0700

    Sentry pod added

---
+++ DiagnosticsTool/Pods/Target Support Files/Pods-DiagnosticsTool-DiagnosticsToolUITests/Pods-DiagnosticsTool-DiagnosticsToolUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiagnosticsTool_DiagnosticsToolUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DiagnosticsTool_DiagnosticsToolUITests
+@end
